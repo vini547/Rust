4c871adf3c7d6b6b4be442f9821dec1b811d548d
vini547 <carlos.cccoimbra@hotmail.com>
2021-09-08 16:20:34 -0300

    Finalização de Tuples

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@
 //mod print;
 //mod vars;
 //mod types;
-  mod strings;
-  
+//mod strings;
+  mod tuples;
 
 fn main() {
 
  //print::run();
  //vars::run();
  //types::run();
-   strings::run();
+ //strings::run();
+   tuples::run();
 
 }
