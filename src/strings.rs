d86e40c64b13038cc0031a6a634a8b31a9d0cc4e
vini547 <carlos.cccoimbra@hotmail.com>
2021-09-08 16:11:40 -0300

    Fialização de Strings.

--- src/strings.rs
+++ src/strings.rs
@@ -33,7 +33,7 @@ pub fn run() {
   println!("Is empty? {}", hello.is_empty());
 
   //CHecar se a varivável contém a palavra hello(Diferencia maiuscula de minuscula
-  println!("Contém a palavra hello? {}
+  println!("Contém a palavra Hello? {}
 ", hello.contains("Hello"));
 
   //Trocar uma palavra específica por uma outra qualquer definida. Repaare que dados da variavel não mudam
@@ -42,9 +42,22 @@ pub fn run() {
 WORLD por BROW 
 HELLO por WHAT'S_UP
 
+
 ==> {} , {}", hello.replace("World", "BROW"), hello.replace("Hello", "WHAT'S_UP"));
   println!("{}", hello);
 
+  //percorrer String e printa-las em linha uma apos a outra
+  for word in hello.split_whitespace() {
+    println!("{}", word);
+  }
 
+ //Criar String a partir de uma capacidade especifica.
+ let mut s = String::with_capacity(10);
+ s.push('a');
+ s.push('b');
+
+ //certificar-se que o comprimento da String é 2 e a capacidade é 10.
+ assert_eq!(2, s.len());
+ assert_eq!(10, s.capacity());
 
 }
